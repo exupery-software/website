c5c58193681a2d929c0cfd4cdbcdbb1fcbbc0be3
Martin Mai <mrkanister1024@googlemail.com>
2020-12-26 11:27:18 +0100

    Redirect 'www' subdomain to bare domain

--- main.go
+++ main.go
@@ -5,13 +5,17 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
 	"time"
 )
 
 func main() {
+	fs := http.FileServer(http.Dir("./www/out"))
+	handler := wwwRedirect(fs)
+
 	srv := &http.Server{
 		Addr:         ":8080",
-		Handler:      http.FileServer(http.Dir("./www/out")),
+		Handler:      handler,
 		IdleTimeout:  30 * time.Second,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
@@ -19,5 +23,19 @@ func main() {
 
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func wwwRedirect(h http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if host := strings.TrimPrefix(r.Host, "www."); host != r.Host {
+			// Request host has www. prefix. Redirect to host with www. trimmed.
+			u := *r.URL
+			u.Host = host
+			u.Scheme = "https"
+			http.Redirect(w, r, u.String(), http.StatusMovedPermanently)
+			return
+		}
+		h.ServeHTTP(w, r)
 	}
 }
